00000b60659ddd38119f0c78e5ae6c036c8edfa6
zeyadalaa <zeyad.alaaeldin1998@gmail.com>
2020-01-20 15:19:24 +0200

    FileCabinet_1: show, index, create, destroy and update

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,48 @@
+class DocsController < ApplicationController
+  before_action :find_doc, only: %i[show update destroy edit]
+  def index
+    @docs = Doc.all
+  end
+
+  def show
+  end
+
+  def new
+    @doc = Doc.new
+  end
+
+  def create
+    @doc = Doc.new(doc_params)
+    if @doc.save
+      redirect_to @doc
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @doc.update(doc_params)
+      redirect_to @doc
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @doc.destroy
+    redirect_to docs_path
+  end
+
+  private
+
+  def find_doc
+    @doc = Doc.find(params[:id])
+  end
+
+  def doc_params
+    params.require(:doc).permit(:title, :content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'docs/index'
+  get 'docs/show'
+  get 'docs/new'
+  get 'docs/create'
+  get 'docs/edit'
+  get 'docs/update'
+  get 'docs/destroy'
   get 'welcome/index'
+  resources :docs
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
